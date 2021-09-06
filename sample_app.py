42fcff7edcd115e0c962b6df1a8a12a425967723
Jeffrey Dunn <53655440+dunnjeffrey@users.noreply.github.com>
2021-09-06 23:06:50 +0800

    Update sample_app.py

--- sample_app.py
+++ sample_app.py
@@ -13,4 +13,5 @@ def calculate():
     return render_template('index.html', bmi=bmi)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
